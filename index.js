b23c097c8303b90c408d4dcc5bb1c24705719d0d
bekji <turayev07@gmail.com>
2020-12-25 22:22:53 +0500

    first version

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import menu from "./menu.js";
 
-// TODO
+menu.addDishToCourse('appetizers', 'Salad1', 20);
+menu.addDishToCourse('appetizers', 'Salad2', 25);
+menu.addDishToCourse('appetizers', 'Salad3', 22);
+
+menu.addDishToCourse('mains', 'Shorva', 30);
+menu.addDishToCourse('mains', 'Lagmon', 35);
+menu.addDishToCourse('mains', 'Osh', 38);
+
+menu.addDishToCourse('desserts', 'Tort', 10);
+menu.addDishToCourse('desserts', 'Paxlava', 8);
+menu.addDishToCourse('desserts', 'Chocolate', 5);
+
+let meal = menu.generateRandomMeal();
+console.log(meal);
--- menu.js
+++ menu.js
@@ -1,5 +1,58 @@
 const menu = {
-  // TODO
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
+  },
+
+  get courses() {
+    return this._courses;
+  },
+
+  addDishToCourse(courseName, dishName, dishPrice) {
+    const dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    switch(courseName){
+      case 'appetizers':
+        this._courses.appetizers.push(dish);
+        break;
+      case 'mains':
+        this._courses.mains.push(dish);
+        break;
+      case 'desserts':
+        this._courses.desserts.push(dish);
+        break;
+    }
+  },
+
+  getRandomDishFromCourse(courseName) {
+    
+    let randomMealIndex = Math.floor(Math.random() * this._courses[courseName].length);
+    if(courseName === 'appetizers'){ 
+      return this._courses.appetizers[randomMealIndex];
+    } else if(courseName === 'mains'){
+      return this._courses.mains[randomMealIndex];
+    } else if(courseName === 'desserts'){
+      return this._courses.desserts[randomMealIndex];
+    }
+  },
+
+  generateRandomMeal() {
+    const appetizer = this.getRandomDishFromCourse('appetizers');
+    const main = this.getRandomDishFromCourse('mains');
+    const dessert = this.getRandomDishFromCourse('desserts');
+
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    
+    const randomMealObject = {
+      dishes: [appetizer.name, main.name, dessert.name],
+      total: '$' + totalPrice
+    };
+    
+    return randomMealObject; 
+  }
 };
 
 export default menu;
