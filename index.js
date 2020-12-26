fe0a4be0cfdf7301ee2836aca8d9faa249e1a439
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2020-12-26 17:56:00 +0300

    app is almost ready

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import menu from "./menu.js";
 
-// TODO
+menu.addDishToCourse('appetizers', 'meal10', 10);
+menu.addDishToCourse('mains', 'meal20', 20);
+menu.addDishToCourse('desserts', 'meal30', 30);
+
+menu.addDishToCourse('appetizers', 'meal11', 11);
+menu.addDishToCourse('mains', 'meal21', 21);
+menu.addDishToCourse('desserts', 'meal31', 31);
+
+menu.addDishToCourse('appetizers', 'meal12', 12);
+menu.addDishToCourse('mains', 'meal22', 22);
+menu.addDishToCourse('desserts', 'meal32', 32);
+
+const meal = menu.generateRandomMeal();
+
+console.log(meal);
--- menu.js
+++ menu.js
@@ -1,5 +1,39 @@
 const menu = {
-  // TODO
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
+  },
+
+  get courses() {
+    return this._courses;
+  },
+
+  addDishToCourse(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice
+    };
+
+    this._courses[courseName].push(dish);
+  },
+
+  getRandomDishFromCourse(courseName) {
+    const randomIndex = Math.floor(Math.random() * this._courses[courseName].length);
+    
+    return this._courses[courseName][randomIndex];
+  },
+  
+  generateRandomMeal() {
+    let appetizer = this.getRandomDishFromCourse('appetizers');
+    let main = this.getRandomDishFromCourse('mains');
+    let dessert = this.getRandomDishFromCourse('desserts');
+    
+    return `dishes: ${appetizer.name}, ${main.name}, ${dessert.name}, 
+    total: ${appetizer.price + main.price + dessert.price}`;
+  }
+  
 };
+
 
 export default menu;
