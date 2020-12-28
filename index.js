cd230aea9866501dcfe2a96cae1bfcbe5539a814
hamidulla-boltaev <hamidulloboltayev@gmail.com>
2020-12-28 21:07:02 +0400

    task 2 is done

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import menu from "./menu.js";
 
-// TODO
+menu.addDishToCourse('appetizers', 'Soup', 5);
+menu.addDishToCourse('mains', 'Fried Meat', 8);
+menu.addDishToCourse('desserts', 'Ice Cream', 5);
+
+menu.addDishToCourse('appetizers', 'Kebab', 12);
+menu.addDishToCourse('mains', 'Somsa', 16);
+menu.addDishToCourse('desserts', 'Cake', 5);
+
+menu.addDishToCourse('appetizers', 'Salad', 7);
+menu.addDishToCourse('mains', 'Plow', 4);
+menu.addDishToCourse('desserts', 'Melon', 2);
+
+let meal = menu.generateRandomMeal();
+console.log(meal);
--- menu.js
+++ menu.js
@@ -1,5 +1,35 @@
 const menu = {
-  // TODO
-};
-
+    _courses : {
+      appetizers : [],
+      mains : [],
+      desserts : []
+      },
+      get courses() {
+        return this._courses;
+      },
+      addDishToCourse(courseName, dishName, dishPrice) {
+        let dish = {
+          name : dishName,
+          price : dishPrice
+        };
+        this._courses[courseName].push(dish);
+      },
+      getRandomDishFromCourse(courseName) {
+        let dishes = this._courses[courseName];
+        let randomIndex = Math.floor(Math.random() * dishes.length);
+        return dishes[randomIndex];
+      },
+      generateRandomMeal() {
+        let appetizer = this.getRandomDishFromCourse('appetizers');
+        let main = this.getRandomDishFromCourse('mains');
+        let dessert = this.getRandomDishFromCourse('desserts');
+        let dishes = [appetizer.name, main.name, dessert.name];
+        let total = appetizer.price + main.price + dessert.price;
+        let  object = {
+          dishes: dishes,
+          total: '$' + total
+        };
+        return object;
+      }
+    };
 export default menu;
