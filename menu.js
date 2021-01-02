621163964245fff026a337ea5302ff5d9a0979a1
hamidulla-boltaev <hamidulloboltayev@gmail.com>
2021-01-02 08:08:13 +0400

    fixed

--- menu.js
+++ menu.js
@@ -1,35 +1,32 @@
 const menu = {
-    _courses : {
-      appetizers : [],
-      mains : [],
-      desserts : []
-      },
-      get courses() {
-        return this._courses;
-      },
-      addDishToCourse(courseName, dishName, dishPrice) {
-        let dish = {
-          name : dishName,
-          price : dishPrice
-        };
-        this._courses[courseName].push(dish);
-      },
-      getRandomDishFromCourse(courseName) {
-        let dishes = this._courses[courseName];
-        let randomIndex = Math.floor(Math.random() * dishes.length);
-        return dishes[randomIndex];
-      },
-      generateRandomMeal() {
-        let appetizer = this.getRandomDishFromCourse('appetizers');
-        let main = this.getRandomDishFromCourse('mains');
-        let dessert = this.getRandomDishFromCourse('desserts');
-        let dishes = [appetizer.name, main.name, dessert.name];
-        let total = appetizer.price + main.price + dessert.price;
-        let  object = {
-          dishes: dishes,
-          total: '$' + total
-        };
-        return object;
-      }
+  _courses : {
+    appetizers : [],
+    mains : [],
+    desserts : []
+  },
+  get courses() {
+    return this._courses;
+  },
+  addDishToCourse(courseName, dishName, dishPrice) {
+    let dish = {
+      name : dishName,
+      price : dishPrice
     };
+    this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName) {
+    let dishes = this._courses[courseName];
+    let randomIndex = Math.floor(Math.random() * dishes.length);
+    return dishes[randomIndex];
+  },
+  generateRandomMeal() {
+    let appetizer = this.getRandomDishFromCourse('appetizers');
+    let main = this.getRandomDishFromCourse('mains');
+    let dessert = this.getRandomDishFromCourse('desserts');
+    return {
+      dishes: [appetizer.name, main.name, dessert.name],
+      total: '$' + (appetizer.price + main.price + dessert.price)
+    };
+  }
+};
 export default menu;
