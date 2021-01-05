33954647dc19cfdd39a41b726a03c8ee6a63ab91
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2021-01-05 20:21:35 +1100

    get your dishes from mealmaker! yay!

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import menu from "./menu.js";
 
-// TODO
+menu.addDishToCourse("appetizers", "egg rolls", 10);
+menu.addDishToCourse("appetizers", "fried mozarella", 12);
+menu.addDishToCourse("appetizers", "crackers", 7);
+
+menu.addDishToCourse("mains", "chicken", 15);
+menu.addDishToCourse("mains", "grilled beef", 18);
+menu.addDishToCourse("mains", "Uyghur noodles", 17);
+
+menu.addDishToCourse("desserts", "ice cream", 5);
+menu.addDishToCourse("desserts", "vanilla milkshake", 6);
+menu.addDishToCourse("desserts", "strawberry smoothie", 5.5);
+
+let meal = menu.generateRandomMeal();
+console.log(meal);
--- menu.js
+++ menu.js
@@ -1,5 +1,38 @@
 const menu = {
-  _courses: {}, // test
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+  },
+
+  get courses() {
+    return this._courses;
+  },
+
+  addDishToCourse(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice,
+    };
+    this._courses[courseName].push(dish);
+  },
+
+  getRandomDishFromCourse(courseName) {
+    let courseDishes = this._courses[courseName];
+    let randomIndex = Math.floor(Math.random() * courseDishes.length);
+    return courseDishes[randomIndex];
+  },
+
+  generateRandomMeal() {
+    let appetizer = this.getRandomDishFromCourse("appetizers");
+    let main = this.getRandomDishFromCourse("mains");
+    let dessert = this.getRandomDishFromCourse("desserts");
+
+    return {
+      dishes: [appetizer.name, main.name, dessert.name],
+      total: "$" + (appetizer.price + main.price + dessert.price),
+    };
+  },
 };
 
 export default menu;
