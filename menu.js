84d14480f69dc22103cb400b27f6922d8f56ebd0
Dilmurod-97 <dilnodirov@gmail.com>
2021-01-06 13:31:02 +0900

    Dilmurod-2

--- menu.js
+++ menu.js
@@ -4,36 +4,31 @@ const menu = {
     mains: [],
     desserts: [],
   },
-    get courses() {
-      return this._courses;
-      },
 
-    addDishToCourse(courseName, dishName, dishPrice) {
-      let dish = {
-        name: dishName,
-        price: dishPrice
-      };
-      this._courses[courseName].push(dish);
-    },
-
-    getRandomDishFromCourse(courseName) {
-      let dishes = this._courses[courseName];
-      let randomIndex = Math.floor(Math.random()*dishes.length);
+  get courses() {
+    return this._courses;
+  },
+  addDishToCourse(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName) {
+    let dishes = this._courses[courseName];
+    let randomIndex = Math.floor(Math.random() * dishes.length);
       return dishes[randomIndex];
-    },
-    
-    generateRandomMeal() {
-      let appetizer = this.getRandomDishFromCourse('appetizers');
-      let main = this.getRandomDishFromCourse('mains');
-      let dessert = this.getRandomDishFromCourse('desserts');
-      let dishes = [appetizer.name, main.name, dessert.name];
-      let total = appetizer.price + main.price + dessert.price;
-      let object = {
-        dishes: dishes,
-        total: '$' + total
-      };
-      return object;
-    }
+  },
+  generateRandomMeal() {
+    let appetizer = this.getRandomDishFromCourse('appetizers');
+    let main = this.getRandomDishFromCourse('mains');
+    let dessert = this.getRandomDishFromCourse('desserts');
+    return {
+      dishes: [appetizer.name, main.name, dessert.name],
+      total: '$' + `${appetizer.price + main.price + dessert.price}`
+    };
+  }
     
 };
 
